d093b8538ab7b66e07c458e15ebc483f69e76114
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2024-01-04 22:25:59 +0100

    Hello World Agent benchmark parameters

--- benchmark/hello/src/test/java/com/jade/HelloWorldAgentTest.java
+++ benchmark/hello/src/test/java/com/jade/HelloWorldAgentTest.java
@@ -9,9 +9,15 @@ import jade.wrapper.StaleProxyException;
 import org.junit.Test;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.fail;
 
@@ -19,6 +25,9 @@ import static org.junit.Assert.fail;
 public class HelloWorldAgentTest {
 
     private AgentContainer container;
+
+    @Param({"1", "2", "5", "10", "50", "100"})
+    private int numberOfAgentsCreated;
 
     public void setup() {
         Runtime runtime = Runtime.instance();
@@ -36,20 +45,29 @@ public class HelloWorldAgentTest {
 
     @Benchmark
     @BenchmarkMode(Mode.SingleShotTime)
+    @Measurement(iterations = 2)
+    @Fork(value = 4)
+    @OutputTimeUnit(TimeUnit.SECONDS)
     @Test
     public void testHelloWorldAgent() {
         // Before
         setup();
 
+        for (int i = 0; i < numberOfAgentsCreated; i++) {
+            createHelloWorldAgent(i);
+        }
+
+        // After
+        teardown();
+    }
+
+    private void createHelloWorldAgent(int agentIndex) {
         try {
-            AgentController agentController = container.createNewAgent("testAgent", HelloWorldAgent.class.getName(), null);
+            AgentController agentController = container.createNewAgent("helloAgent" + agentIndex, HelloWorldAgent.class.getName(), null);
             agentController.start();
         } catch (StaleProxyException e) {
             e.printStackTrace();
             fail("Test failed: " + e.getMessage());
         }
-
-        // After
-        teardown();
     }
 }
