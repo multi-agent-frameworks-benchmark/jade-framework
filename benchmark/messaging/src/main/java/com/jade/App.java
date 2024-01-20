ed2b5ce989cd9467b065bac871b26b71f7eca9d4
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2024-01-20 14:14:40 +0100

    Build to fatJar that include JHM.

--- benchmark/messaging/src/main/java/com/jade/App.java
+++ benchmark/messaging/src/main/java/com/jade/App.java
@@ -1,5 +1,23 @@
 package com.jade;
 
+import com.jade.benchmark.MessagingBenchmarkPath;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.text.SimpleDateFormat;
+
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws Exception {
+        String dateRun = new SimpleDateFormat("yyyy.MM.dd.HH.mm").format(new java.util.Date());
+
+        Options options = new OptionsBuilder()
+                .include(MessagingBenchmarkPath.class.getSimpleName())
+                .output("../" + dateRun + "-benchmark-result-hello.txt")
+                .resultFormat(ResultFormatType.TEXT)
+                .build();
+
+        new Runner(options).run();
+    }
 }
